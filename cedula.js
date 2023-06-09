3e7f60362f6efeee655e0cde72ec4e6d240a8ecb
vric948 <vric948@hotmail.com>
2023-06-09 08:17:07 -0700

    Algoritmos commit

---
+++ cedula.js
@@ -0,0 +1,10 @@
+let FN = 1996;
+let FA = 2023;
+let EDAD;
+EDAD = FA - FN
+
+if (EDAD > 17) {
+    console.log("Es necesario que saques tu cédula de identidad.");
+} else {
+    console.log("No es necesario que saques tu cédula de identidad.");
+};
---
+++ cedula.py
@@ -0,0 +1,9 @@
+FN = 1996
+FA = 2023
+
+EDAD = FN - FA
+
+if EDAD > 17:
+  print("Necesitas sacar tu cédula de identidad")
+else:
+  print("Aún no necesitas sacar tu cédula")
